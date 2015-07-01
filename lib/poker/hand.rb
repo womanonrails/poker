83d230e969df4d27ffa5e5e34a2cf1aa43e76d90
Agnieszka Matysek <amatysek@fractalsoft.org>
2015-07-01 19:37:06 +0200

    Add fields to class and fix tests after that

--- lib/poker/hand.rb
+++ lib/poker/hand.rb
@@ -3,71 +3,74 @@ require 'pry'
 module Poker
   # Poker hand
   class Hand
-    def check(array)
-      array.sort!
-      return 9 if straight_flush?(array)
-      return 8 if four_of_a_kind?(array)
-      return 7 if full_house?(array)
-      return 6 if flush?(array)
-      return 5 if straight?(array)
-      return 4 if three_of_a_kind?(array)
-      return 3 if two_pair?(array)
-      return 2 if one_pair?(array)
-      return 1 if high_card?(array)
+    def initialize(array)
+      @array = array.sort
+      @carts = @array.map { |item| item / 4 }
+    end
+
+    def check
+      return 9 if straight_flush?
+      return 8 if four_of_a_kind?
+      return 7 if full_house?
+      return 6 if flush?
+      return 5 if straight?
+      return 4 if three_of_a_kind?
+      return 3 if two_pair?
+      return 2 if one_pair?
+      return 1 if high_card?
       return 0
     end
 
-    def straight_flush?(array)
-      straight?(array) && flush?(array)
+    def straight_flush?
+      straight? && flush?
     end
 
-    def four_of_a_kind?(array)
+    def four_of_a_kind?
       hash = Hash.new(0)
-      array.each { |item| hash[item / 4] += 1 }
+      @carts.each { |item| hash[item] += 1 }
       hash.values.include?(4)
     end
 
-    def full_house?(array)
-      three_of_a_kind?(array) && one_pair?(array)
+    def full_house?
+      three_of_a_kind? && one_pair?
     end
 
-    def flush?(array)
-      color = array.map { |item| item % 4 }
+    def flush?
+      color = @array.map { |item| item % 4 }
       color.uniq.size == 1
     end
 
-    def normal_straight?(array)
-      array.each_cons(2) do |previous, current|
+    def normal_straight?
+      @carts.each_cons(2) do |previous, current|
         return false unless previous + 1 == current
       end
       true
     end
 
-    def straight?(array)
-      carts = array.map { |item| item / 4 }
-      [0, 1, 2, 3, 12] == carts || normal_straight?(carts)
+    def straight?
+      [0, 1, 2, 3, 12] == @carts || normal_straight?
     end
 
-    def three_of_a_kind?(array)
+    def three_of_a_kind?
       hash = Hash.new(0)
-      array.each { |item| hash[item / 4] += 1 }
+      @carts.each { |item| hash[item] += 1 }
       hash.values.include?(3)
     end
 
-    def two_pair?(array)
+    def two_pair?
       hash = Hash.new(0)
-      array.each { |item| hash[item / 4] += 1 }
+      @carts.each { |item| hash[item] += 1 }
       (hash.values - [2]).size == 1
     end
 
-    def one_pair?(array)
+    def one_pair?
       hash = Hash.new(0)
-      array.each { |item| hash[item / 4] += 1 }
+      @carts.each { |item| hash[item] += 1 }
       hash.values.include?(2)
     end
 
-    def high_card?(array)
-      array.last >= 36
+    def high_card?
+      @array.last >= 36
     end
   end
 end
--- spec/poker/hand_spec.rb
+++ spec/poker/hand_spec.rb
@@ -2,169 +2,189 @@ require 'spec_helper'
 
 describe Poker::Hand do
   it 'generates cards properly' do
-    cards = subject.check([0, 1, 2, 3, 4])
-    expect(cards).not_to be_nil
+    hand = described_class.new([0, 1, 2, 3, 4])
+    expect(hand.check).not_to be_nil
   end
 
-  it 'detects straight_flush' do
-    #karty po kolei
-    expect(subject.check([0, 4, 8, 12, 16])).to eq(9)
-    expect(subject.check([5, 9, 13, 17, 21])).to eq(9)
-    expect(subject.check([10, 14, 18, 22, 26])).to eq(9)
-    expect(subject.check([15, 19, 23, 27, 31])).to eq(9)
-    expect(subject.check([16, 20, 24, 28, 32])).to eq(9)
-
-    #karty zrandomizowane
-    expect(subject.check([37, 29, 33, 21, 25].shuffle)).to eq(9)
-    expect(subject.check([30, 34, 38, 26, 42].shuffle)).to eq(9)
-    expect(subject.check([31, 35, 47, 43, 39].shuffle)).to eq(9)
-    expect(subject.check([32, 48, 40, 44, 36].shuffle)).to eq(9)
-    expect(subject.check([49, 45, 41, 37, 33].shuffle)).to eq(9)
+  [
+    [0, 4, 8, 12, 16],
+    [5, 9, 13, 17, 21],
+    [10, 14, 18, 22, 26],
+    [15, 19, 23, 27, 31],
+    [16, 20, 24, 28, 32],
+    [37, 29, 33, 21, 25].shuffle,
+    [30, 34, 38, 26, 42].shuffle,
+    [31, 35, 47, 43, 39].shuffle,
+    [32, 48, 40, 44, 36].shuffle,
+    [49, 45, 41, 37, 33].shuffle
+  ].each do |carts|
+    it "detects straight_flush for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 9
+    end
   end
 
-  it 'detects four_of_a_kind' do
-    #karty po kolei
-    expect(subject.check([0, 1, 2, 3, 4])).to eq(8)
-    expect(subject.check([4, 5, 6, 0, 7])).to eq(8)
-    expect(subject.check([8, 9, 0, 10, 11])).to eq(8)
-    expect(subject.check([12, 0, 13, 14, 15])).to eq(8)
-    expect(subject.check([0, 16, 17, 18, 19])).to eq(8)
-
-    #karty zrandomizowane
-    expect(subject.check([20, 21, 22, 23, 0].shuffle)).to eq(8)
-    expect(subject.check([24, 25, 26, 27, 0].shuffle)).to eq(8)
-    expect(subject.check([28, 29, 30, 31, 0].shuffle)).to eq(8)
-    expect(subject.check([32, 33, 34, 35, 0].shuffle)).to eq(8)
-    expect(subject.check([36, 37, 38, 39, 0].shuffle)).to eq(8)
+  [
+    [0, 1, 2, 3, 4],
+    [4, 5, 6, 0, 7],
+    [8, 9, 0, 10, 11],
+    [12, 0, 13, 14, 15],
+    [0, 16, 17, 18, 19],
+    [20, 21, 22, 23, 0].shuffle,
+    [24, 25, 26, 27, 0].shuffle,
+    [28, 29, 30, 31, 0].shuffle,
+    [32, 33, 34, 35, 0].shuffle,
+    [36, 37, 38, 39, 0].shuffle
+  ].each do |carts|
+    it "detects four_of_a_kind for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 8
+    end
   end
 
-  it 'detects full_house' do
-    #karty po kolei
-    expect(subject.check([1, 2, 3, 4, 5])).to eq(7)
-    expect(subject.check([5, 6, 7, 8, 9])).to eq(7)
-    expect(subject.check([9, 10, 11, 12, 13])).to eq(7)
-    expect(subject.check([13, 14, 15, 16, 17])).to eq(7)
-    expect(subject.check([17, 18, 19, 20, 21])).to eq(7)
-
-    #karty zrandomizowane
-    expect(subject.check([21, 22, 23, 24, 25].shuffle)).to eq(7)
-    expect(subject.check([25, 26, 27, 28, 29].shuffle)).to eq(7)
-    expect(subject.check([30, 31, 32, 33, 34].shuffle)).to eq(7)
-    expect(subject.check([34, 35, 36, 37, 38].shuffle)).to eq(7)
-    expect(subject.check([38, 39, 40, 41, 42].shuffle)).to eq(7)
+  [
+    [1, 2, 3, 4, 5],
+    [5, 6, 7, 8, 9],
+    [9, 10, 11, 12, 13],
+    [13, 14, 15, 16, 17],
+    [17, 18, 19, 20, 21],
+    [21, 22, 23, 24, 25].shuffle,
+    [25, 26, 27, 28, 29].shuffle,
+    [30, 31, 32, 33, 34].shuffle,
+    [34, 35, 36, 37, 38].shuffle,
+    [38, 39, 40, 41, 42].shuffle
+  ].each do |carts|
+    it "detects full_house for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 7
+    end
   end
 
-  it 'detects flush' do
-    #karty po kolei
-    expect(subject.check([0, 4, 8, 12, 20])).to eq(6)
-    expect(subject.check([5, 9, 13, 17, 25])).to eq(6)
-    expect(subject.check([10, 14, 18, 22, 30])).to eq(6)
-    expect(subject.check([15, 19, 23, 27, 35])).to eq(6)
-    expect(subject.check([16, 20, 24, 28, 36])).to eq(6)
-
-    #karty zrandomizowane
-    expect(subject.check([21, 25, 29, 33, 41].shuffle)).to eq(6)
-    expect(subject.check([26, 30, 2, 6, 14].shuffle)).to eq(6)
-    expect(subject.check([3, 7, 15, 51, 39].shuffle)).to eq(6)
-    expect(subject.check([2, 50, 22, 30, 10].shuffle)).to eq(6)
-    expect(subject.check([49, 41, 29, 17, 13].shuffle)).to eq(6)
+  [
+    [0, 4, 8, 12, 20],
+    [5, 9, 13, 17, 25],
+    [10, 14, 18, 22, 30],
+    [15, 19, 23, 27, 35],
+    [16, 20, 24, 28, 36],
+    [21, 25, 29, 33, 41].shuffle,
+    [26, 30, 2, 6, 14].shuffle,
+    [3, 7, 15, 51, 39].shuffle,
+    [2, 50, 22, 30, 10].shuffle,
+    [49, 41, 29, 17, 13].shuffle
+  ].each do |carts|
+    it "detects flush for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 6
+    end
   end
 
-  it 'detects straight' do
-    #karty po kolei
-    expect(subject.check([0, 5, 10, 15, 18])).to eq(5)
-    expect(subject.check([0, 5, 10, 15, 48])).to eq(5)
-    expect(subject.check([4, 9, 14, 19, 22])).to eq(5)
-    expect(subject.check([8, 13, 18, 23, 26])).to eq(5)
-    expect(subject.check([12, 17, 22, 27, 30])).to eq(5)
-    expect(subject.check([16, 21, 26, 31, 34])).to eq(5)
-
-    #karty zrandomizowane
-    expect(subject.check([49, 46, 43, 38, 33].shuffle)).to eq(5)
-    expect(subject.check([1, 6, 9, 15, 50].shuffle)).to eq(5)
-    expect(subject.check([45, 42, 39, 34, 29].shuffle)).to eq(5)
-    expect(subject.check([41, 38, 35, 30, 25].shuffle)).to eq(5)
-    expect(subject.check([37, 34, 31, 26, 21].shuffle)).to eq(5)
-    expect(subject.check([34, 31, 26, 21, 16].shuffle)).to eq(5)
+  [
+    [0, 5, 10, 15, 18],
+    [0, 5, 10, 15, 48],
+    [4, 9, 14, 19, 22],
+    [8, 13, 18, 23, 26],
+    [12, 17, 22, 27, 30],
+    [16, 21, 26, 31, 34],
+    [49, 46, 43, 38, 33].shuffle,
+    [1, 6, 9, 15, 50].shuffle,
+    [45, 42, 39, 34, 29].shuffle,
+    [41, 38, 35, 30, 25].shuffle,
+    [37, 34, 31, 26, 21].shuffle,
+    [34, 31, 26, 21, 16].shuffle
+  ].each do |carts|
+    it "detects straight for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 5
+    end
   end
 
-  it 'detects three_of_a_kind' do
-    #karty po kolei
-    expect(subject.check([0, 1, 2, 4, 8])).to eq(4)
-    expect(subject.check([4, 5, 6, 8, 12])).to eq(4)
-    expect(subject.check([8, 9, 10, 12, 16])).to eq(4)
-    expect(subject.check([12, 13, 14, 16, 20])).to eq(4)
-    expect(subject.check([16, 17, 18, 20, 24])).to eq(4)
-
-    #karty zrandomizowane
-    expect(subject.check([20, 21, 22, 24, 28].shuffle)).to eq(4)
-    expect(subject.check([24, 25, 26, 28, 32].shuffle)).to eq(4)
-    expect(subject.check([28, 29, 30, 32, 37].shuffle)).to eq(4)
-    expect(subject.check([32, 33, 34, 0, 4].shuffle)).to eq(4)
-    expect(subject.check([51, 50, 49, 0, 14].shuffle)).to eq(4)
+  [
+    [0, 1, 2, 4, 8],
+    [4, 5, 6, 8, 12],
+    [8, 9, 10, 12, 16],
+    [12, 13, 14, 16, 20],
+    [16, 17, 18, 20, 24],
+    [20, 21, 22, 24, 28].shuffle,
+    [24, 25, 26, 28, 32].shuffle,
+    [28, 29, 30, 32, 37].shuffle,
+    [32, 33, 34, 0, 4].shuffle,
+    [51, 50, 49, 0, 14].shuffle
+  ].each do |carts|
+    it "detects three_of_a_kind for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 4
+    end
   end
 
-  it 'detects two_pair' do
-    #karty po kolei
-    expect(subject.check([0, 1, 4, 5, 8])).to eq(3)
-    expect(subject.check([4, 5, 8, 9, 12])).to eq(3)
-    expect(subject.check([8, 9, 12, 13, 16])).to eq(3)
-    expect(subject.check([12, 13, 16, 17, 20])).to eq(3)
-    expect(subject.check([16, 17, 20, 21, 24])).to eq(3)
-
-    #karty zrandomizowane
-    expect(subject.check([20, 21, 24, 25, 28].shuffle)).to eq(3)
-    expect(subject.check([24, 25, 28, 29, 32].shuffle)).to eq(3)
-    expect(subject.check([28, 29, 32, 33, 37].shuffle)).to eq(3)
-    expect(subject.check([32, 33, 36, 37, 40].shuffle)).to eq(3)
-    expect(subject.check([36, 37, 40, 41, 44].shuffle)).to eq(3)
+  [
+    [0, 1, 4, 5, 8],
+    [4, 5, 8, 9, 12],
+    [8, 9, 12, 13, 16],
+    [12, 13, 16, 17, 20],
+    [16, 17, 20, 21, 24],
+    [20, 21, 24, 25, 28].shuffle,
+    [24, 25, 28, 29, 32].shuffle,
+    [28, 29, 32, 33, 37].shuffle,
+    [32, 33, 36, 37, 40].shuffle,
+    [36, 37, 40, 41, 44].shuffle
+  ].each do |carts|
+    it "detects two_pair for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 3
+    end
   end
 
-  it 'detects pair' do
-    #karty po kolei
-    expect(subject.check([0, 7, 8, 36, 37])).to eq(2)
-    expect(subject.check([0, 40, 41, 44, 48])).to eq(2)
-    expect(subject.check([0, 8, 12, 44, 45])).to eq(2)
-    expect(subject.check([1, 9, 13, 48, 50])).to eq(2)
-    expect(subject.check([7, 19, 22, 38, 39])).to eq(2)
-
-    #karty zrandomizowane
-    expect(subject.check([46, 47, 51, 43, 0].shuffle)).to eq(2)
-    expect(subject.check([46, 44, 16, 48, 0].shuffle)).to eq(2)
-    expect(subject.check([49, 50, 0, 7, 9].shuffle)).to eq(2)
-    expect(subject.check([37, 39, 1, 13, 21].shuffle)).to eq(2)
-    expect(subject.check([48, 49, 0, 39, 18].shuffle)).to eq(2)
+  [
+    [0, 7, 8, 36, 37],
+    [0, 40, 41, 44, 48],
+    [0, 8, 12, 44, 45],
+    [1, 9, 13, 48, 50],
+    [7, 19, 22, 38, 39],
+    [46, 47, 51, 43, 0].shuffle,
+    [46, 44, 16, 48, 0].shuffle,
+    [49, 50, 0, 7, 9].shuffle,
+    [37, 39, 1, 13, 21].shuffle,
+    [48, 49, 0, 39, 18].shuffle
+  ].each do |carts|
+    it "detects pair for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 2
+    end
   end
 
-  it 'detects high_card' do
-    #karty po kolei
-    expect(subject.check([0, 7, 8, 12, 37])).to eq(1)
-    expect(subject.check([0, 40, 19, 44, 48])).to eq(1)
-    expect(subject.check([0, 8, 12, 19, 45])).to eq(1)
-    expect(subject.check([1, 9, 13, 19, 50])).to eq(1)
-    expect(subject.check([7, 19, 22, 1, 39])).to eq(1)
-
-    #karty zrandomizowane
-    expect(subject.check([46, 21, 51, 43, 0].shuffle)).to eq(1)
-    expect(subject.check([46, 21, 16, 48, 0].shuffle)).to eq(1)
-    expect(subject.check([49, 21, 0, 7, 9].shuffle)).to eq(1)
-    expect(subject.check([2, 39, 29, 13, 21].shuffle)).to eq(1)
-    expect(subject.check([48, 21, 0, 39, 18].shuffle)).to eq(1)
+  [
+    [0, 7, 8, 12, 37],
+    [0, 40, 19, 44, 48],
+    [0, 8, 12, 19, 45],
+    [1, 9, 13, 19, 50],
+    [7, 19, 22, 1, 39],
+    [46, 21, 51, 43, 0].shuffle,
+    [46, 21, 16, 48, 0].shuffle,
+    [49, 21, 0, 7, 9].shuffle,
+    [2, 39, 29, 13, 21].shuffle,
+    [48, 21, 0, 39, 18].shuffle
+  ].each do |carts|
+    it "detects high_card for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 1
+    end
   end
 
-  it 'detects nothing' do
-    #karty po kolei
-    expect(subject.check([0, 10, 18, 24, 34])).to eq(0)
-    expect(subject.check([4, 24, 22, 28, 8])).to eq(0)
-    expect(subject.check([8, 28, 26, 32, 2])).to eq(0)
-    expect(subject.check([12, 32, 19, 6, 1])).to eq(0)
-    expect(subject.check([16, 29, 34, 0, 9])).to eq(0)
-
-    #karty zrandomizowane
-    expect(subject.check([0, 10, 18, 24, 34].shuffle)).to eq(0)
-    expect(subject.check([4, 24, 22, 28, 8].shuffle)).to eq(0)
-    expect(subject.check([8, 28, 26, 32, 2].shuffle)).to eq(0)
-    expect(subject.check([12, 0, 19, 34, 6].shuffle)).to eq(0)
-    expect(subject.check([16, 0, 34, 20, 9].shuffle)).to eq(0)
+  [
+    [0, 10, 18, 24, 34],
+    [4, 24, 22, 28, 8],
+    [8, 28, 26, 32, 2],
+    [12, 32, 19, 6, 1],
+    [16, 29, 34, 0, 9],
+    [0, 10, 18, 24, 34].shuffle,
+    [4, 24, 22, 28, 8].shuffle,
+    [8, 28, 26, 32, 2].shuffle,
+    [12, 0, 19, 34, 6].shuffle,
+    [16, 0, 34, 20, 9].shuffle
+  ].each do |carts|
+    it "detects high_card for #{carts}" do
+      hand = described_class.new(carts)
+      expect(hand.check).to eq 0
+    end
   end
 end
