c50429e114a8346c0a05e3fddda9b8af112d3552
Agnieszka Matysek <amatysek@fractalsoft.org>
2017-05-23 19:56:12 +0200

    Add new rule for flush

---
+++ lib/rules/flushness_rule.rb
@@ -0,0 +1,11 @@
+module Rules
+  class FlushnessRule
+    def initialize(color_array)
+      @color_array = color_array
+    end
+
+    def check?
+      @color_array.uniq.size == 1
+    end
+  end
+end
---
+++ spec/rules/flushness_rule_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Rules::FlushnessRule do
+  it 'check if array has one color' do
+    flushness_rule = described_class.new([1, 1, 1, 1])
+    expect(flushness_rule).to be_check
+  end
+
+  it 'check if array has not one color' do
+    flushness_rule = described_class.new([2, 3, 2, 2])
+    expect(flushness_rule).to_not be_check
+  end
+end
